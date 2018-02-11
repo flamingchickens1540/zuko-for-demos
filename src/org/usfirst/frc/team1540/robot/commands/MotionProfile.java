b04c46ce563c8a0d51db87b659f49057f70ea3ea
edelmanjm <edelmanjm@users.noreply.github.com>
2018-02-11 14:39:15 -0800

    Move back to time based from position
    Too much complexity and too many issues with position.

--- src/org/usfirst/frc/team1540/robot/commands/MotionProfile.java
+++ src/org/usfirst/frc/team1540/robot/commands/MotionProfile.java
@@ -46,8 +46,7 @@ public class MotionProfile extends Command {
     for (Properties currentProperty : motionProfiles) {
       // Each controller's setpoint is calculated at a slightly different time, but this doesn't
       // matter, since the motion profile is "continuous."
-      double velocity = getPostionSetpoint(currentProperty, timer.get(), lastTime,
-          currentProperty.getEncoderTickRatio());
+      double velocity = getVelocitySetpoint(currentProperty, timer.get(), lastTime);
       SmartDashboard.putNumber("calculatedVelocity", velocity);
       currentProperty.getSetMotorVelocityFunction().accept(velocity);
     }
@@ -55,8 +54,8 @@ public class MotionProfile extends Command {
     lastTime = timer.get();
   }
 
-  private double getPostionSetpoint(Properties currentProperty, double currentTime,
-      double lastTime, double encoderMultiplier) {
+  private double getVelocitySetpoint(Properties currentProperty, double currentTime,
+      double lastTime) {
 
     /*
       Whoa! This is weird. Although everything is ordered base on time, that's prone to getting off
@@ -71,19 +70,31 @@ public class MotionProfile extends Command {
 
     Trajectory thisTrajectory = currentProperty.getTrajectory();
     double dt = thisTrajectory.segments[0].dt;
+    double encoderMultiplier = currentProperty.getEncoderTickRatio();
+    double quadraturePosition = currentProperty.getGetEncoderPositionFunction().getAsDouble();
+
 
     // Start from the current time and find the closest point.
     int startIndex = Math.toIntExact(Math.round(currentTime / dt));
 
     SmartDashboard.putNumber("startIndex", startIndex);
 
-    // FIXME
+    // Moved away from this because of the additional complexity and unsolved problems
+    // Maybe we can take this back up later
     /*
+    double calculatedVelocity;
+
     // Linear search is fine since we don't expect to be going far and the overhead of a different
     // data structure or search algorithm probably isn't worth it.
     for (int d = 0; ; d++) {
 
-      SmartDashboard.putNumber("currentSearchIndex", d);
+      // Check if we're at the end
+      if (startIndex + d >= thisTrajectory.segments.length - 1) {
+        calculatedVelocity = thisTrajectory.segments[thisTrajectory.segments.length - 1].velocity;
+        break;
+      }
+
+      SmartDashboard.putNumber("sIndexDelta", d);
       // Set it either to the correct place or first item
       Segment loSegment = (startIndex - d >= 0 ? thisTrajectory.segments[startIndex - d]
           : thisTrajectory.segments[0]);
@@ -100,37 +111,48 @@ public class MotionProfile extends Command {
           : thisTrajectory.segments[thisTrajectory.length() - 1]);
 
       // Grab the position, otherwise we might have issues where neither is true
-      double position = currentController.getQuadraturePosition() * encoderMultiplier;
+      double position = quadraturePosition * encoderMultiplier;
 
+
+      // TODO Getting stuck with the velocity at index 0
       // If the target position is between the last point's and this point's position
       // Take the one that's the least far ahead in time, then linearly interpolate between the two
       // to find the target velocity
       if (isBetween(position, loSegment.position, loSegmentn.position)
           && (startIndex - d) * dt > lastTime) {
         // Target the low segment; make sure it's actually ahead of the last time.
-        return linearInterpolation(position, loSegment.position, loSegment.velocity,
+        calculatedVelocity = linearInterpolation(position, loSegment.position, loSegment.velocity,
             loSegmentn.position, loSegmentn.velocity);
+        break;
       } else if (isBetween(position, hiSegmentn.position, hiSegment.position)) {
         // Target the high segment
-        return linearInterpolation(position, hiSegmentn.position, hiSegmentn.velocity,
+        calculatedVelocity = linearInterpolation(position, hiSegmentn.position, hiSegmentn.velocity,
             hiSegment.position, hiSegment.velocity);
+        break;
+      } else if (isBetween(position, 0, loSegment.position)) {
+        // If we haven't even made it to the first index
+        calculatedVelocity = loSegment.velocity;
+        break;
       } else if (startIndex + d > thisTrajectory.length() - 1
           || currentTime > thisTrajectory.length() * dt) {
         isFinished = true;
         // If we've overrun the end, set the velocity to be the end velocity.
-        return thisTrajectory.segments[thisTrajectory.length() - 1].velocity;
+        calculatedVelocity = thisTrajectory.segments[thisTrajectory.length() - 1].velocity;
+        break;
       }
     }
+
+    return calculatedVelocity / encoderMultiplier * 0.1;
     */
 
+    // Very simple but reliable implementation commented out.
     int length = thisTrajectory.segments.length;
     int index = (startIndex < length) ? startIndex : length - 1;
-    // FIXME math
     return thisTrajectory.segments[index].velocity / encoderMultiplier * 0.1;
   }
 
   /**
-   * Calculate if a number is between two other numbers
+   * Calculate if a number is between (inclusive) two other numbers
    *
    * @param x The target
    * @param x1 Lower bound
